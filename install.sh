2b0dc3ea7d1a02a6cd4472653d8b6749248cee31
Sameer Naik <sameer@damagehead.com>
2015-06-28 15:33:12 +0530

    install.sh: added status messages

--- install.sh
+++ install.sh
@@ -12,29 +12,49 @@ apt-get install -y gcc make yasm bzip2 \
 # use all available processor cores for the build
 alias make="make -j$(nproc)"
 
-# compile rtmpsuck
+# RTMPSuck
+echo "Downloading rtmpdump..."
+wget "http://repo.or.cz/w/rtmpdump.git/snapshot/HEAD.tar.gz" -O /tmp/rtmpdump.tar.gz
+
+echo "Extracting rtmpdump..."
 mkdir -p /tmp/rtmpdump
-wget "http://repo.or.cz/w/rtmpdump.git/snapshot/HEAD.tar.gz" -O - | tar -zxf - -C /tmp/rtmpdump --strip=1
+tar -zxf /tmp/rtmpdump.tar.gz -C /tmp/rtmpdump --strip=1
+
+echo "Building rtmpdump..."
 cd /tmp/rtmpdump/
 sed 's,prefix=/usr/local,prefix=/usr,' -i Makefile
 sed 's,prefix=/usr/local,prefix=/usr,' -i librtmp/Makefile
 make && make install
 
-# compile x264
+# x264
+echo "Downloading x264..."
 wget "http://ftp.videolan.org/pub/x264/snapshots/x264-${X264_VERSION}.tar.bz2" -O /tmp/x264-${X264_VERSION}.tar.bz2
+
+echo "Extracting x264..."
 mkdir -p /tmp/x264
+tar -jxf /tmp/x264-${X264_VERSION}.tar.bz2 -C /tmp/x264 --strip=1
+
+echo "Building x264..."
 cd /tmp/x264 && ./configure --prefix=/usr --enable-shared --disable-opencl
 make && make install
 
-# compile ffmpeg
+# FFMpeg
+echo "Downloading ffmpeg..."
+wget "http://www.ffmpeg.org/releases/ffmpeg-${FFMPEG_VERSION}.tar.bz2" -O /tmp/ffmpeg-${FFMPEG_VERSION}.tar.bz2
+
+echo "Extracting ffmpeg..."
 mkdir -p /tmp/ffmpeg/
-wget "http://www.ffmpeg.org/releases/ffmpeg-${FFMPEG_VERSION}.tar.bz2" -O - | tar -jxf - -C /tmp/ffmpeg/ --strip=1
+tar -jxf /tmp/ffmpeg-${FFMPEG_VERSION}.tar.bz2 -C /tmp/ffmpeg/ --strip=1
+
+echo "Building ffmpeg..."
 cd /tmp/ffmpeg/
 ./configure --prefix=/usr --disable-static --enable-shared --enable-gpl --enable-nonfree \
   --enable-libx264 --enable-libmp3lame --enable-libvpx --enable-librtmp --enable-yasm \
   --enable-ffmpeg --enable-ffplay --enable-ffserver --enable-network --enable-gnutls \
   --enable-libopus --disable-debug --enable-libvo-aacenc --enable-version3
 make && make install
+
+echo "Cleaning up..."
 
 # purge build dependencies, don't need 'em anymore
 apt-get purge -y --auto-remove gcc make yasm bzip2 \
